d50007fc9a641ac7a5e053e0a7ebb9497eecc3ff
BelyashMatvey <belyakov06mat@mail.ru>
2020-04-21 19:40:29 +0300

    scientific programs

---
+++ maminaproga1.py
@@ -0,0 +1,70 @@
+from PIL import Image
+from PIL import ImageTk
+import tkinter as tk
+import math
+import time
+import random
+import pyautogui as pya
+def clicker():
+    pya.click(x=500,y=500,clicks=5,interval=2.0)
+deg=0
+def window1():
+    poetry="Эта программа написана исключительно в научных целях.\nПоэтому копирование и использование \nэтой программы производится только с раз-\nрешения правообладателя.\n\n\t\tПравообладатель-Belyash."
+    window=tk.Tk()
+    window.title("Правила пользования")
+    label2 = tk.Label(text=poetry, font="Courier 20")
+    label2.place(relx=.2, rely=.3)
+    btn=tk.Button(text="Я принимаю условия пользования", background="#555", foreground="#ccc",padx="20", pady="8", font="16", command=lambda:window.destroy())
+    btn.pack()
+    label2.pack()
+    window.mainloop()
+def window2():
+    global root, line_2, canv, px, py, ox, oy,ox1,error
+    root = tk.Tk()
+    canv = tk.Canvas(root, width=600, height=400, bg="white")
+    line_1=canv.create_line(330,250,260,150,width=3)
+    px, py, ox, oy = 300,250,random.randint(210,290),150
+    if 230>ox:
+        error=str(int(230-ox))+"-"
+    else:
+        error=str(int(ox-230))+"+"
+    line_2 = canv.create_line(px, py, ox, oy, width=4, fill="grey")
+    root.title("Эксперимент")
+    canv.focus_set()
+    canv.pack()
+    root.after(120000,lambda x=1:root.destroy())
+    f.write(str(px)+" "+str(py)+"     "+str(ox)+" "+str(oy))
+    root.bind("<space>", lambda event: quit(root))
+    canv.bind("<Right>", lambda event: turner('right'))
+    canv.bind("<Left>", lambda event: turner('left'))
+    root.mainloop()
+def turner(angle):
+    deg = 0
+    global px,py,ox,oy,ox1
+    if angle=='right':
+        deg=deg-0.25
+        theta = math.radians(deg)
+        cosang, sinang = math.cos(theta), math.sin(theta)
+    elif angle=='left':
+        deg=deg+0.25
+        theta=math.radians(deg)
+        cosang, sinang = math.cos(theta), math.sin(theta)
+    tx, ty = ox - px, oy - py
+    ox = (tx * cosang + ty * sinang) + px
+    global error
+    if 230>ox:
+        error=str(int(230-ox))+"-"
+    else:
+        error=str(int(ox-230))+"+"
+    #oy = (-tx * sinang + ty * cosang) + py
+    global line_2
+    canv.coords(line_2, px, py, ox, oy)
+    print('px = ', px, 'py = ', py, 'ox = ', ox, 'oy = ', oy)
+f=open("output1.txt","w")
+window1()
+for i in range(2):
+    window2()
+    f.write("\n"+str(round(px))+" "+str(round(py))+"     "+str(round(ox))+" "+str(round(oy))+"      "+error+"\n"+"\n"+"\n"+"\n")
+
+
+f.close()
